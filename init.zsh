d3df821a92bd765b8ae7a2bce044323ae67a84b4
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-11-08 22:26:38 -0500

    version

---
+++ init.zsh
@@ -0,0 +1,8 @@
+p6df::modules::p6test::version()        { echo "0.0.1"; }
+p6df::modules::p6test::init() {
+
+  local dir="$P6_DFZ_SRC_DIR/p6m7g8/p6test"
+
+  . $dir/lib/_bootstrap.sh
+  p6_p6test_bootstrap "$dir"
+}
